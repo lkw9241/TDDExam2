d1d24f6c0e73927fe69286274c03b9f67520a628
LeeKW <lkw9241@gmail.com>
2025-06-05 10:25:48 +0900

    step 1, g

---
+++ src/main/java/com/back/Rq.java
@@ -0,0 +1,11 @@
+package com.back;
+
+public class Rq {
+    public Rq(String cmd){
+        
+    }
+
+    public String getActionName() {
+        return "수정";
+    }
+}
--- src/test/java/com/back/RqTest.java
+++ src/test/java/com/back/RqTest.java
@@ -8,11 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RqTest {
     @Test
     @DisplayName("rq.getActionName()")
-    void t1(){
+    void t1() {
         Rq rq = new Rq("수정?id=1");
 
         String actionName = rq.getActionName();
 
         assertEquals("수정", actionName);
     }
+}
 
